68e29bbf5dee3b96bf7d9bb8f283f25f5cbb7ccf
itsphucuong <lpcuong8888@gmail.com>
2023-12-20 23:00:51 +0700

    Cap nhat tinh nang Extract de kiem tra ket qua bai lam

--- heapplayer.py
+++ heapplayer.py
@@ -1,4 +1,3 @@
-
 import sys
 import argparse
 from colorama import Fore, Style, init
@@ -43,6 +42,23 @@ def min_heapify(arr, n, i):
     if smallest != i:
         arr[i], arr[smallest] = arr[smallest], arr[i]
         min_heapify(arr, n, smallest)
+def extract_max(arr):
+    if len(arr) == 0:
+        return None
+    max_item = arr[0]
+    arr[0] = arr[-1]
+    arr.pop()
+    max_heapify(arr, len(arr), 0)
+    return max_item
+
+def extract_min(arr):
+    if len(arr) == 0:
+        return None
+    min_item = arr[0]
+    arr[0] = arr[-1]
+    arr.pop()
+    min_heapify(arr, len(arr), 0)
+    return min_item
 
 def main(args):
     heap_steps = []
@@ -68,11 +84,22 @@ def main(args):
     print(Fore.GREEN + f"\nBuilding a {heap_type}:" + Style.RESET_ALL)
     print(tabulate(enumerate(heap_steps, 1), headers=["Step", "Heap"], tablefmt="fancy_grid"))
 
+
+    # Xử lý extract heap
+    if args.extract:
+        if heap_type == "Max Heap":
+            extracted = extract_max(current_heap)
+        elif heap_type == "Min Heap":
+            extracted = extract_min(current_heap)
+        print(Fore.YELLOW + f"\nExtracted element: {extracted}" + Style.RESET_ALL)
+        heap_steps.append(list(current_heap))
+        print(tabulate(enumerate(heap_steps, 1), headers=["Step", "Heap"], tablefmt="fancy_grid"))
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Build a max or min heap from a list of numbers.")
     parser.add_argument("-M", "--max", nargs='+', type=int, help="Build a max heap from the given numbers.")
     parser.add_argument("-m", "--min", nargs='+', type=int, help="Build a min heap from the given numbers.")
-
+    parser.add_argument("-e", "--extract", action="store_true", help="Extract the top element from the heap after building it.")
     args = parser.parse_args()
     main(args)
 
